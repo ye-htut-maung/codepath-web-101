ef8d9e88aec879c4a34751036632dae38f2f40d6
ye-htut-maung <yehtutmaung21@gmail.com>
2023-11-04 00:25:03 -0400

    Finish Unit 7

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const sign_now = document.querySelector(".signatures");
 const input_name = document.querySelector("#name");
 const input_hometown = document.querySelector("#hometown");
 const input_email = document.querySelector("#email");
-const submit_sign_now_btn = document.querySelector("#sign-now-button");
+const signNowButton = document.querySelector("#sign-now-button");
 
 // Add your query for the sign now button here
 
@@ -32,4 +32,36 @@ const addSignature = () => {
 };
 
 // Add a click event listener to the sign now button here
-submit_sign_now_btn.addEventListener("click", addSignature);
+// submit_sign_now_btn.addEventListener("click", addSignature);
+
+// TODO: Remove the click event listener that calls addSignature()
+
+// TODO: Complete validation form
+
+const validateForm = () => {
+  let containsErrors = false;
+
+  var petitionInputs = document.getElementById("sign-petition").elements;
+  // TODO: Loop through all inputs
+  for (let i = 0; i < petitionInputs.length; i++) {
+    if (petitionInputs[i].value.length < 2) {
+      containsErrors = true;
+      petitionInputs[i].classList.add("error");
+    } else {
+      petitionInputs[i].classList.remove("error");
+    }
+  }
+  if (containsErrors == false) {
+    addSignature();
+    for (let i = 0; i < petitionInputs.length; i++) {
+      petitionInputs[i].value = "";
+      containsErrors = false;
+    }
+  }
+
+  // TODO: Validate the value of each input
+
+  // TODO: Call addSignature() and clear fields if no errors
+};
+
+signNowButton.addEventListener("click", validateForm);
